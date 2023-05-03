50ef69fc6c9ed294334f8cceeaa07d7c2c25ef82
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-05-03 19:49:13 +0300

    gave the relationships between the models

---
+++ app/models/restaurant_pizza.rb
@@ -0,0 +1,5 @@
+class RestaurantPizza < ApplicationRecord
+  belongs_to :restaurant
+  belongs_to :pizza
+  validates :price, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 30 }
+end
